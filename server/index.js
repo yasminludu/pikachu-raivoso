4d5e90ad8827a5d9255d4747f086375377044c49
Yasmin Luduvice <luduvice55@gmail.com>
2025-03-22 00:02:53 -0300

    feat: envio de dado para servidor

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import path from 'path'
 
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     const projectPath = path.dirname(import.meta.url)
@@ -10,6 +12,13 @@ app.get('/', function(req, res) {
     let pagePath = path.join(projectPath, '/index.html')
     pagePath = pagePath.replace('file:/', '/')
     res.sendFile(pagePath)
+})
+
+app.post('/register', function(req, res) {
+    console.log('tentando registrar usuário')
+    console.log(req.body)
+    req.params.name
+    res.send('')
 })
 
 app.listen(3000, '0.0.0.0',function() {
