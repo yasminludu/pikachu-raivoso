ea8694f7f25acb45932521eac7a83e324dded1d8
Yasmin Luduvice <luduvice55@gmail.com>
2025-03-25 13:50:18 -0300

    criacao de leitura de ficheiro csv

---
+++ readcsv.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+fs.createReadStream("./usuarios.csv")
+	.pipe(parse({ delimiter: ",", from_line: 2 }))
+	.on("data", function (row) {
+	  console.log(row);
+	})
+	.on("end", function (){
+	  console.log("finished");
+	})
+	.on("error", function (error) {
+	  console.log(error.message);
+	});
+
