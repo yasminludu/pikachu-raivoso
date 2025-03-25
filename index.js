8b7bebd88d23149a0de95ab2d81de1947d325dd2
Yasmin Luduvice <luduvice55@gmail.com>
2025-03-25 13:56:03 -0300

    tentei salvar os dados do cadastro em CSV e nao deu certo, 'endereco do arquivo nao encontrado' ou algo do tipo aparece na tela

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
 app.get('/', function(req, res) {
     const projectPath = path.dirname(import.meta.url)
     let pagePath = path.join(projectPath, '/index.html')
@@ -12,7 +15,7 @@ app.get('/', function(req, res) {
     res.sendFile(pagePath)
 })
 
-app.post('/register', function(req, res) {
+app.post('/usuarios.csv', function(req, res) {
     console.log('tentando registrar usuário')
     console.log(req.body)
    // salve conteudo do body em arquivo.
@@ -31,3 +34,36 @@ app.get('/thanks', (req, res) => {
 app.listen(3000, '0.0.0.0',function() {
     console.log('Escutando na porta 3000')
 })
+
+
+//CSV
+fs.createReadStream(".usuarios.csv")
+    .pipe(parse({ delimiter: ",", from_line: 2 }))
+    .on("data", function (row) {
+        console.log(row);
+    })
+    .on("end", function() {
+        console.log("finished");
+    })
+    .on("error", function (error){
+        console.log(error.message);
+    });
+
+fs.readFile('/home/yasmin/code/pikachu-raivoso/usuarios.csv', 'utf-8', (err,data) => {
+    if (err){
+        console.error('Erro ao ler o ficheiro:', err);
+        return;
+    }
+    console.log('Conteudo do ficheiro CSV:', data);
+});
+
+parse(data, {
+    columns: true,
+    delimiter: ',',
+}, (err, records) => {
+    if (err) {
+        console.error('Erro ao analisar o ficheiro CSV:', err);
+        return;
+    }
+    console.log('Dados do usuario analisados:', records);
+});
